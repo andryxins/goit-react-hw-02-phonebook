c9d0e2487574f66c321bc1140432e56467c28a6b
andryxins <andryxins@icloud.com>
2020-04-10 20:09:36 +0300

    Updates

---
+++ precache-manifest.1526f4c62711767ca89e294984002ac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41d7bb968e9c33fb75f18d52c7bd088",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "00f57532e199502a2b74",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.68791d8f.chunk.css"
+  },
+  {
+    "revision": "30c98234c55ea0b4e0f8",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.038987fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.038987fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f57532e199502a2b74",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.093907d7.chunk.js"
+  },
+  {
+    "revision": "bfccad9aa0892efebfa9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.98a8e906.js"
+  }
+]);
